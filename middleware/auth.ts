817687eac95d8a9324fea9bf85113fafb9a6c152
Shardul <shardul@WILASSETA0175.local>
2024-01-04 12:44:06 +0530

    fix: comment auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,22 +1,22 @@
-import { Response, Request, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
+// import { Response, Request, NextFunction } from 'express'
+// import jwt from 'jsonwebtoken'
 
-const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
-  try {
-    const token = req.header('accessToken')
-    if (!token) {
-      return res.status(401).json({
-        error: true,
-        message: 'token is required for authentication',
-      })
-    }
+// const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
+//   try {
+//     const token = req.header('accessToken')
+//     if (!token) {
+//       return res.status(401).json({
+//         error: true,
+//         message: 'token is required for authentication',
+//       })
+//     }
 
-    const decode = jwt.verify(token, process.env.SECRET_KEY || ' ')
-    req.body.decode = decode
-    next()
-  } catch (error: any) {
-    return res.status(500).send({ error: true, message: error.message })
-  }
-}
+//     const decode = jwt.verify(token, process.env.SECRET_KEY || ' ')
+//     req.body.decode = decode
+//     next()
+//   } catch (error: any) {
+//     return res.status(500).send({ error: true, message: error.message })
+//   }
+// }
 
-export default verifyToken
+// export default verifyToken
