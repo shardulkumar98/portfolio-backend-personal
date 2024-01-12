31e359094ccd0a9f4d7d04ec11179a9af4663c29
Shardul <shardul@WILASSETA0175.local>
2024-01-12 17:39:48 +0530

    fix: add body parser

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import router from './routes'
 
 const createServer = (): express.Application => {
@@ -10,6 +11,9 @@ const createServer = (): express.Application => {
   app.use(express.json())
   app.use(cors())
   app.use(express.urlencoded({ extended: true }))
+  app.use(bodyParser.json({ limit: '50mb' }))
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+
   app.use('/api', router)
   app.use(cookieParser())
 
